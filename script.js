d2aeb2e503331b8d2e101f981d53cd89fba7cd4a
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-10-04 19:09:46 -0400

    IDed necessary HTML elements, minor CSS changes

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 import fetch from "node-fetch";
+
+const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+const data = await response.json();
+
+document.getElementById("picture").src= searchPic.src;
