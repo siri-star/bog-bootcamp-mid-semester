cfdcfc98c3949885ced760cba8266c7140769e05
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-10-05 01:21:45 -0400

    functional arrow buttons, displaying name and picture

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-import fetch from "node-fetch";
+let id = 1;
+let total = 0;
 
-const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-const data = await response.json();
+async function update() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
+    const data = await response.json();
 
-document.getElementById("picture").src= searchPic.src;
+    document.getElementById("picture").src = data.sprites.other["official-artwork"].front_default;
+    document.getElementById("name").textContent = data.forms[0].name;
+}
+
+async function initialize() {
+    const allPokemonResponse = await fetch('https://pokeapi.co/api/v2/ability/?limit=0');
+    const allPokemonData = await allPokemonResponse.json();
+    total = allPokemonData.count;
+    update();
+}
+
+document.getElementById("back").addEventListener(
+    "click",
+    () => {
+        // TODO: unintuitive, use modular arithmatic
+        id = id - 1 < 1 ? total : id - 1;
+        update();
+});
+
+document.getElementById("forward").addEventListener(
+    "click",
+    () => {
+        // TODO: unintuitive, use modular arithmatic
+        id = id + 1 > total ? 1 : id + 1;
+        update();
+});
+
+initialize();
